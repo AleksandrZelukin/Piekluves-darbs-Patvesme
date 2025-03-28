1af52561ffc56893f0b98704aff17b185b1db321
Aleksandr Zelukin <92846541+AleksandrZelukin@users.noreply.github.com>
2025-03-28 19:56:56 +0200

    Update apps

--- main.py
+++ main.py
@@ -103,4 +103,4 @@ def palidziba():
   return render_template('palidziba.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0',debug=True, port=5050)
